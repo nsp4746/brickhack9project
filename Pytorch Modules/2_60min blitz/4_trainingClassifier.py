df77b049f840307eb53b6c7976e08e875c041ced
nsp4746 <nikhilpat67@gmail.com>
2023-02-27 12:03:05 -0500

    Pushing so I can work on desktop

--- Pytorch Modules/2_60min blitz/4_trainingClassifier.py
+++ Pytorch Modules/2_60min blitz/4_trainingClassifier.py
@@ -22,102 +22,165 @@ Step 1. Load and normailze the CIFAR10 training set
 
 # Importing the required libraries
 
+if __name__ == "__main__":
 
-import torch.nn.functional as F
-import torch.nn as nn
-import numpy as np
-import matplotlib.pyplot as plt
-import torch
-import torchvision
-import torchvision.transforms as transforms
-transform = transforms.Compose([transforms.ToTensor(),
-                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])  # Normalizing the data
-batch_size = 4
+    import torch.optim as optim
+    import torch.nn.functional as F
+    import torch.nn as nn
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import torch
+    import torchvision
+    import torchvision.transforms as transforms
+    transform = transforms.Compose([transforms.ToTensor(),
+                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])  # Normalizing the data
+    batch_size = 4
 
-trainset = torchvision.datasets.CIFAR10(
-    root='./data',
-    train=True,
-    download=True,
-    transform=transform)
-# The data is downloaded in the folder ./data, time to load it
-trainloader = torch.utils.data.DataLoader(trainset,
-                                          batch_size=batch_size,
-                                          shuffle=True,
-                                          num_workers=2)
-# The data is loaded in the trainloader variable
-# The data is shuffled and the batch size is 4
+    trainset = torchvision.datasets.CIFAR10(
+        root='./data',
+        train=True,
+        download=True,
+        transform=transform)
+    # The data is downloaded in the folder ./data, time to load it
+    trainloader = torch.utils.data.DataLoader(trainset,
+                                              batch_size=batch_size,
+                                              shuffle=True,
+                                              num_workers=2)
+    # The data is loaded in the trainloader variable
+    # The data is shuffled and the batch size is 4
 
-# It's nw time to loader the test data set
-trainset = torchvision.daasets.CIFAR10(
-    root='./data',
-    train=False,
-    download=True,
-    transform=transform
-)
-# The data is downloaded in the folder ./data, time to load it
-testLoader = torch.utils.data.DataLoader(trainset,
-                                         batch_size=batch_size,
-                                         shuffle=False,
-                                         num_workers=2)
-# The data is loaded in the testLoader variable
+    # It's nw time to loader the test data set
+    trainset = torchvision.datasets.CIFAR10(
+        root='./data',
+        train=False,
+        download=True,
+        transform=transform
+    )
+    # The data is downloaded in the folder ./data, time to load it
+    testLoader = torch.utils.data.DataLoader(trainset,
+                                             batch_size=batch_size,
+                                             shuffle=False,
+                                             num_workers=2)
+    # The data is loaded in the testLoader variable
 
-classes = (
-    'plane',
-    'car',
-    'bird',
-    'cat',
-    'deer',
-    'dog',
-    'frog',
-    'horse',
-    'ship',
-    'truck'
-)
-# Loaded into a tuple
+    classes = (
+        'plane',
+        'car',
+        'bird',
+        'cat',
+        'deer',
+        'dog',
+        'frog',
+        'horse',
+        'ship',
+        'truck'
+    )
+    # Loaded into a tuple
 
-# Lets make so we can see some of the training images
-# Import the required libraries
+    # Lets make so we can see some of the training images
+    # Import the required libraries
 
-# write function to show an image
-
-
-def imageShow(img):
-    img = img/2 + 0.5
-    npimg = img.numpy()
-    plt.imshow(np.transpose(1, 2, 0))
-    plt.show()
+    # write function to show an image
 
 
-dataiter = iter(trainloader)  # get random images
-images, labels = next(dataiter)
-
-# show the images
-imageShow(torchvision.utils.make_grid(images))
-# print labels
-print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
-
-# 2. Define a convulational neural network
-# Import the required libraries
+    def imageShow(img):
+        img = img/2 + 0.5
+        npimg = img.numpy()
+        plt.imshow(np.transpose(npimg,(1, 2, 0)))
+        plt.show()
 
 
-class Net(nn.Module):
-    def __init__(self):
-        super().__init__()
-        # 3 input channels, 6 output channels, 5x5 kernel
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)  # 2x2 kernel
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+    dataiter = iter(trainloader)  # get random images
+    images, labels = next(dataiter)
 
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1) # flatten all the dimensions except the batch size
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+    # show the images
+    imageShow(torchvision.utils.make_grid(images))
+    # print labels
+    print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
 
-net = Net()
+    # 2. Define a convulational neural network
+    # Import the required libraries
+
+
+    class Net(nn.Module):
+        def __init__(self):
+            super().__init__()
+            # 3 input channels, 6 output channels, 5x5 kernel
+            self.conv1 = nn.Conv2d(3, 6, 5)
+            self.pool = nn.MaxPool2d(2, 2)  # 2x2 kernel
+            self.conv2 = nn.Conv2d(6, 16, 5)
+            self.fc1 = nn.Linear(16 * 5 * 5, 120)
+            self.fc2 = nn.Linear(120, 84)
+            self.fc3 = nn.Linear(84, 10)
+
+        def forward(self, x):
+            x = self.pool(F.relu(self.conv1(x)))
+            x = self.pool(F.relu(self.conv2(x)))
+            # flatten all the dimensions except the batch size
+            x = torch.flatten(x, 1)
+            x = F.relu(self.fc1(x))
+            x = F.relu(self.fc2(x))
+            x = self.fc3(x)
+            return x
+
+
+    net = Net()
+
+
+    # 3. Define a loss function and optimizer
+    # Using Classification Cross-Entropy loss and SGD with momentum
+    # import the required librariers
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    # lr is the learning rate, momentum is the momentum term
+
+    # 4. Train the network
+    # This is where the magic happens. Simply loop over the data iteraltor and feed the inputs to the network and optimizer
+
+    for epoch in range(2):
+
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            inputs, labels = data
+            optimizer.zero_grad()
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            if i % 2000 == 1999:  # print every 2000 mini-batches
+                print(f'[{epoch+1}, {i+1}] loss: {running_loss/2000:.3f}')
+                running_loss = 0.0
+    print("finished tracking")
+
+    PATH = './cifar_net.pth'
+    torch.save(net.state_dict(), PATH)
+
+    # 5. Test the network on the test data
+    # We have trained the network for 2 passes over the training dataset.
+    dataiter = iter(testLoader)
+    images, labels = next(dataiter)
+    # print images
+    imageShow(torchvision.utils.make_grid(images))
+    print('GroundTruth: ', ' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
+
+    net = Net()
+    net.load_state_dict(torch.load(PATH))
+
+    outputs = net(images)
+    _, predicted = torch.max(outputs, 1)
+    print('Predicted: ', ' '.join(f'{classes[predicted[j]]:5s}' for j in range(batch_size)))
+
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in testLoader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    print(f'Accuracy of the network on the 10000 test images: {100*correct/total:.2f}%')
+
